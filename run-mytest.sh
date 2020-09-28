7d58f52827fa642d14f244bf2834ed9ecd9d3849
Sergio Takahashi <sergio.takahashi@b2wdigital.com>
2020-09-28 17:15:06 -0300

    Ajustando porta do myzuul para 8086

--- run-mytest.sh
+++ run-mytest.sh
@@ -41,7 +41,7 @@ $(docker run \
     myservice)
 
 $(docker run \
-    -e JAVA_OPTS="-DEUREKA_URI=http://myeureka -DAPP_NAME=myzuul -DSERVER_PORT=8080 -DEUREKA_PORT=8761" \
+    -e JAVA_OPTS="-DEUREKA_URI=http://myeureka -DAPP_NAME=myzuul -DSERVER_PORT=8086 -DEUREKA_PORT=8761" \
     -d \
     --name myzuul \
     --link myeureka \
